ac48b936735b55ee625aa58fb77ad4a95bff8fe8
Ruslan Yupyn <r.yupyn@crossroadlabs.xyz>
2017-02-21 16:56:55 +0200

    added proj file for library building;; added Cartfile for library dependencies; modified .gitignore file;

---
+++ Stencil/Stencil.h
@@ -0,0 +1,19 @@
+//
+//  Stencil.h
+//  Stencil
+//
+//  Created by Ruslan Yupyn on 2/21/17.
+//  Copyright © 2017 CrossroadLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stencil.
+FOUNDATION_EXPORT double StencilVersionNumber;
+
+//! Project version string for Stencil.
+FOUNDATION_EXPORT const unsigned char StencilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stencil/PublicHeader.h>
+
+
